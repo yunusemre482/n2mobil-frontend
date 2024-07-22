c508acf5a0d3797b0dfb6206385aa8fc69444fe4
Yunus emre akgün <akgunyunus482@gmail.com>
2024-07-22 13:42:04 +0300

    chore: Update postcss.config.js, vue.config.js, .prettierrc, babel.config.js, and package.json

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-	presets: ['@vue/cli-plugin-babel/preset'],
+    presets: ["@vue/cli-plugin-babel/preset"],
 
-	// disable decorators before syntax error is fixed
-	plugins: [['@babel/plugin-proposal-decorators', { legacy: true }]],
-}
+    // disable decorators before syntax error is fixed
+    plugins: [["@babel/plugin-proposal-decorators", { legacy: true }]],
+};
