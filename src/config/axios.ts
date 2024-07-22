80a71cba9c832a3b067bcaf3e6a5dc81b079d21d
Yunus emre akgün <akgunyunus482@gmail.com>
2024-07-22 12:22:27 +0300

    chore: Update axios configuration
    This commit updates the axios configuration in the `axios.ts` file. It includes the following changes:
    - Removed unnecessary imports and code formatting
    - Updated the API_URL variable to use the VUE_APP_API_BASE_URL environment variable
    - Refactored the request interceptor to handle the authorization header
    - Refactored the response interceptor to handle token refreshing
    
    These changes improve the configuration and functionality of the axios module.

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,57 +1,56 @@
-import axios from 'axios';
+import axios from 'axios'
 
-const API_URL = process.env.VUE_APP_API_BASE_URL ?? "https://jsonplaceholder.typicode.com"
-
+const API_URL =
+	process.env.VUE_APP_API_BASE_URL ?? 'https://jsonplaceholder.typicode.com'
 
 const api = axios.create({
-    baseURL: API_URL,
-});
-
+	baseURL: API_URL,
+})
 
 api.interceptors.request.use(
-    function (config) {
-        try {
-            const accessToken = localStorage.getItem("accessToken");
-            config.headers['authorization'] = `Bearer ${accessToken}`;
-        } catch (error) {
-            console.log(error);
-        }
-        // Do something before request is sent
-        return config;
-    }, function (error) {
-        // Do something with request error
-        return Promise.reject(error);
-    }
-
-);
+	function (config) {
+		try {
+			const accessToken = localStorage.getItem('accessToken')
+			config.headers['authorization'] = `Bearer ${accessToken}`
+		} catch (error) {
+			console.log(error)
+		}
+		// Do something before request is sent
+		return config
+	},
+	function (error) {
+		// Do something with request error
+		return Promise.reject(error)
+	},
+)
 
 // Interceptors
 api.interceptors.response.use(
-    (config) => {
-        console.log(config);
-        return config;
-    },
-    async (error) => {
-        const originalRequest = error.config;
-        if (
-            error.response.status === 401 &&
-            originalRequest &&
-            !originalRequest._isRetry
-        ) {
-            originalRequest.isRetry = true;
-            try {
-                const response = await axios.get(`${API_URL}/refresh-token`);
+	(config) => {
+		console.log(config)
+		return config
+	},
+	async (error) => {
+		const originalRequest = error.config
+		if (
+			error.response.status === 401 &&
+			originalRequest &&
+			!originalRequest._isRetry
+		) {
+			originalRequest.isRetry = true
+			try {
+				const response = await axios.get(`${API_URL}/refresh-token`)
 
-                const data = await response.data;
-                localStorage.setItem('sessionToken', data);
-               
-                return api.request(originalRequest);
-            } catch (err) {
-                console.error(error.message);
-            }
-        }
-        throw error;
-    }
-);
+				const data = await response.data
+				localStorage.setItem('sessionToken', data)
 
-export default api;
+				return api.request(originalRequest)
+			} catch (err) {
+				console.error(error.message)
+			}
+		}
+		throw error
+	},
+)
+
+export default api
