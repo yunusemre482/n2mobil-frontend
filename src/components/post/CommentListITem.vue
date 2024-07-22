e770deee1138522d2e2c4c808b3b835ba921c6d3
Yunus emre akgün <akgunyunus482@gmail.com>
2024-07-22 12:21:58 +0300

    feat: Add new components for todo and post management
    This commit adds new components for managing todos and posts. It includes the following changes:
    - Added TodoList.vue component for displaying a list of todos
    - Added PostList.vue component for displaying a list of posts
    - Added PostDetail.vue component for displaying the details of a post
    - Added CommentList.vue component for displaying a list of comments on a post
    - Added PostListItem.vue component for displaying a single post item
    - Added TodoListItem.vue component for displaying a single todo item
    - Added SideBarItem.vue component for displaying a sidebar item
    - Added SideBar.vue component for displaying a sidebar navigation menu
    
    These new components will enhance the functionality and user experience of the application.

---
+++ src/components/post/CommentListITem.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>
+		<h1>Todo</h1>
+		<p>This is the todo page</p>
+	</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
---
+++ src/components/sidebar/SideBarItem.vue
@@ -0,0 +1,22 @@
+<template>
+	<li class="nav-item">
+		<a
+			class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+			href="#pablo"
+		>
+			<i
+				class="fab fa-pinterest text-lg leading-lg text-white opacity-75"
+			/>
+			<span class="ml-2">Pin</span>
+		</a>
+	</li>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+	title?: string
+	rout?: string
+}>()
+</script>
+
+<style></style>
