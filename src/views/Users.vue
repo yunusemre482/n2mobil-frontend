e2049de69d1987440ed6986fb60077b80862fbb6
Yunus emre akgün <akgunyunus482@gmail.com>
2024-07-22 12:22:19 +0300

    feat: Add Todos.vue and update Users.vue
    The changes include adding a new component Todos.vue for managing todos and updating the Users.vue component. This enhances the functionality and user experience of the application.

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,42 +1,37 @@
 <template>
-  <div>
-    <h1>Users</h1>
-    <p>This is the users page</p>
-  </div>
+	<div>
+		<h1>Users</h1>
+		<p>This is the users page</p>
+	</div>
 </template>
 
 <script>
-    import { mapGetters } from "vuex";
-    import store from "@/store";
-    import {
-        FETCH_USERS,
-    } from "@/store/actions";
+import { mapGetters } from 'vuex'
+import store from '@/store'
+import { FETCH_USERS } from '@/store/actions'
 
-
-    export default {
-        name: "Users",
-        computed: {
-            ...mapGetters({
-                users: "users",
-            }),
-        },
-        beforeRouteEnter (to, from, next) {
-            // ...
-            Promise.all([
-                store.dispatch(`users/${FETCH_USERS}`),
-            ]).then(() => {
-                next();
-            });
-        },
-        methods: {
-            fetchUsers() {
-                store.dispatch(FETCH_USERS);
-            },
-        },
-        created() {
-            this.fetchUsers();
-        },
-    };
+export default {
+	name: 'Users',
+	computed: {
+		...mapGetters({
+			users: 'users',
+		}),
+	},
+	beforeRouteEnter(to, from, next) {
+		// ...
+		Promise.all([store.dispatch(`${FETCH_USERS}`)]).then(() => {
+			next()
+		})
+	},
+	methods: {
+		fetchUsers() {
+			store.dispatch(FETCH_USERS)
+		},
+	},
+	created() {
+		this.fetchUsers()
+	},
+}
 </script>
 
 <style></style>
