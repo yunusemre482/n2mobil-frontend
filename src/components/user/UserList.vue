1a8a8f8f3dd41544eb37a35fec8bd392ed89af55
Yunus emre akgün <akgunyunus482@gmail.com>
2024-07-23 19:20:00 +0300

    refactor: Update component styles for AlbumListCard and UserCard
    This commit updates the component styles for AlbumListCard.vue and UserCard.vue to use a consistent width of 24rem instead of the previous value of 358.67px. This change improves the responsiveness and visual appearance of the cards.

--- src/components/user/UserList.vue
+++ src/components/user/UserList.vue
@@ -1,6 +1,6 @@
 <template>
     <ul
-        class="container mx-auto lg:gap-10 flex-wrap flex sm:gap-5 items-center lg:justify-start justify-center"
+        class="container mx-auto lg:gap-10 flex-wrap flex md:gap-4 sm:gap-5 items-center justify-start"
     >
         <li v-for="user in users" :key="user.id">
             <UserCard :user="user"/>
